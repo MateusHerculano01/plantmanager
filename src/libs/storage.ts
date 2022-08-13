0ae32baefe940b1b8ae3ec6d0232a09a73f09873
Mateus Herculano <mateuherculano0@gmail.com>
2022-08-13 15:31:38 -0300

    chore: push notifications

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -24,6 +24,22 @@ export interface StoragePlantProps {
 
 export async function savePlant(plant: PlantProps): Promise<void> {
   try {
+    const nextTime = new Date(plant.dateTimeNotification);
+    const now = new Date();
+
+    const { repeat_every, times } = plant.frequency;
+
+    if (repeat_every === 'week') {
+      const interval = Math.trunc(7 / times);
+      nextTime.setDate(now.getDate() + interval);
+    } else {
+      nextTime.setDate(nextTime.getDate() + 1);
+    }
+
+    const seconds = Math.abs(
+      Math.ceil(now.getTime() - nextTime.getTime() / 1000)
+    );
+
     const data = await AsyncStorage.getItem('@plantmanager:plants');
     const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
